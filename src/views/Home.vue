a0a8a69f15d3aadc0dd116ecc14e5e3ca67fb89f
hummingengineer <hummingengineer@gmail.com>
2020-01-12 23:41:22 +0900

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,16 @@
 <template>
-  <v-item-group multiple>
-    <v-container>
+  <v-container>
+    <v-row>
+      <v-col>
+        <div class="text-center display-1"><span style="color: red">{{ currentPlayer }}</span> 플레이어의 차례입니다</div>
+      </v-col>
+    </v-row>
+
+    <v-item-group multiple>
       <v-row v-for="row in 3" :key="row">
         <v-col v-for="col in 3" :key="col">
           <v-item v-slot:default="{ active, toggle }">
-            <v-card :color="active ? 'primary' : ''" class="d-flex align-center" dark height="200" @click="toggle">
+            <v-card :color="active ? 'primary' : ''" class="d-flex align-center" dark height="200" @click="toggle(); clickSquare()">
               <v-scroll-y-transition>
                 <div v-if="active" class="display-3 flex-grow-1 text-center">
                   {{ currentPlayer }}
@@ -14,8 +20,9 @@
           </v-item>
         </v-col>
       </v-row>
-    </v-container>
-  </v-item-group>
+    </v-item-group>
+
+  </v-container>
 </template>
 
 <script>
@@ -25,6 +32,12 @@ export default {
   data: function () {
     return {
       currentPlayer: 'X'
+    }
+  },
+
+  methods: {
+    clickSquare: function () {
+      this.currentPlayer === 'X' ? this.currentPlayer = 'O' : this.currentPlayer = 'X'
     }
   }
 
