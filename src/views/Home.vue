81b259e5717d2a2842095331af05b9d39b087e69
hummingengineer <hummingengineer@gmail.com>
2020-01-14 23:52:38 +0900

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -11,7 +11,7 @@
       <v-row v-for="row in 3" :key="row">
         <v-col v-for="col in 3" :key="col">
           <v-item v-slot:default="{ active, toggle }">
-            <v-card :color="active ? 'primary' : ''" class="d-flex align-center" dark height="200" @click="toggle(); clickSquare(row, col)">
+            <v-card :color="active ? 'primary' : ''" class="d-flex align-center" dark height="200" :disabled="squares[row - 1][col - 1] ? true : false" @click="clickSquare(row, col, toggle)">
               <v-scroll-y-transition>
                 <div v-if="active" class="display-3 flex-grow-1 text-center">
                   {{ squares[row - 1][col - 1] }}
@@ -22,6 +22,17 @@
         </v-col>
       </v-row>
     </v-item-group>
+
+    <v-dialog v-model="dialog" max-width="400" persistent>
+      <v-card>
+        <v-card-title>{{ winPlayer }} 플레이어가 승리하였습니다!</v-card-title>
+        <v-card-actions>
+          <v-btn color="green darken-1" text @click="dialog = false">
+            확인
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
 
   </v-container>
 </template>
@@ -37,14 +48,21 @@ export default {
         Array(3).fill(null),
         Array(3).fill(null),
         Array(3).fill(null)
-      ]
+      ],
+      dialog: false,
+      winPlayer: null
     }
   },
 
   methods: {
-    clickSquare: function (row, col) {
+    clickSquare: function (row, col, toggle) {
       this.squares[row - 1][col - 1] = this.currentPlayer
+      if (this.isWin()) this.dialog = true
+      toggle()
       this.currentPlayer === 'X' ? this.currentPlayer = 'O' : this.currentPlayer = 'X'
+    },
+    isWin: function () {
+      //
     }
   }
 
